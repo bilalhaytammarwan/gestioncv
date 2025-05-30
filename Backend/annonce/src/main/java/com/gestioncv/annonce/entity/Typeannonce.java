d1f24f6c8b5383d9b3c152906db315ddf62d6641
gold <m.soukrati7242@uca.ac.ma>
2025-05-30 17:48:16 +0100

    delete replicase

--- Backend/annonce/src/main/java/com/gestioncv/annonce/entity/Typeannonce.java
+++
@@ -1,5 +0,0 @@
-package com.gestioncv.annonce.entity;
-
-public enum Typeannonce {
-    User,Entreprise;
-}
--- Backend/attachement/src/main/java/attachementmodule/types/AttachementType.java
+++
@@ -1,7 +0,0 @@
-package attachementmodule.types;
-
-public enum AttachementType {
-    IMAGE,
-    LEGAL_DOCUMENT,
-    CV
-}
